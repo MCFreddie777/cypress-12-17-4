0a46c8b819731f7e0a2c8ce145f5d88693f6da5d
František Gič <mcfreddie777@gmail.com>
2023-08-18 15:10:12 +0200

    Use webpack-preprocessor

--- cypress/support/plugins/index.ts
+++ cypress/support/plugins/index.ts
@@ -1,9 +1,30 @@
 /// <reference types="cypress" />
-const webpackPreprocessor = require('@cypress/webpack-batteries-included-preprocessor');
+import * as path from 'path';
+
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+const webpackOptions = {
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+						options: {
+							configFile: path.resolve(__dirname, '../../tsconfig.json'),
+						},
+					},
+				],
+			},
+		],
+	},
+};
 
 // @ts-ignore-next-line no-unused-vars
 module.exports = (on: Cypress.PluginEvents, config:Cypress.PluginConfigOptions) => {
-    on('file:preprocessor', webpackPreprocessor({
-        typescript: require.resolve('typescript')
-    }));
+	on('file:preprocessor', webpackPreprocessor({webpackOptions}));
 };
